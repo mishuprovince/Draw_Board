1b3cf88365070a59e0c519d123a319ea46f23817
xlovelteb <qishi5261@live.cn>
2020-02-27 04:54:44 +0800

    budil layout

--- app/src/main/java/com/example/drawboard/DrawBoard.java
+++ app/src/main/java/com/example/drawboard/DrawBoard.java
@@ -1,14 +1,90 @@
 package com.example.drawboard;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DrawBoard extends View {
+
+    private Paint mpaint=new Paint();
+    private int strokeWidth;
+    private Canvas mcanvas;
+    private Path path;
+    private float mLastX;
+    private float mLastY;
+    private List<Path> pathList = new ArrayList<>();
+    private int mWidth;
+    private int mHeight;
+    private Bitmap mBufferBitmap;
+
     public DrawBoard(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
+    private void init(Context context) {
+        mcanvas = new Canvas();
+        mpaint.setColor(Color.BLACK);
+        mpaint.setStyle(Paint.Style.STROKE);
+        mpaint.setStrokeWidth(20);
+        mpaint.setAntiAlias(true);
+        mpaint.setStrokeCap(Paint.Cap.ROUND);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        for (Path path1 : pathList) {
+            canvas.drawPath(path1, mpaint);
+        }
+        canvas.save();
+        canvas.restore();
+        Log.i("pathlist",""+pathList.size());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX(), y = event.getY();
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            path = new Path();
+            pathList.add(path);
+            path.moveTo(event.getX(), event.getY());
+            mLastX = x;
+            mLastY = y;
+            invalidate();
+        }
+        if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            path.quadTo(mLastX, mLastY, (mLastX + x) / 2, (mLastY + y) / 2);
+            mLastX = x;
+            mLastY = y;
+            invalidate();
+        }
+        if (event.getAction()==MotionEvent.ACTION_UP){
+            path.quadTo(mLastX, mLastY, (mLastX + x) / 2, (mLastY + y) / 2);
+            mcanvas.drawPath(path, mpaint);
+            mLastX = x;
+            mLastY = y;
+            invalidate();
+        }
+        return true;
+    }
 }
--- app/src/main/java/com/example/drawboard/MainActivity.java
+++ app/src/main/java/com/example/drawboard/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.drawboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.security.PrivateKey;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioGroup menu;
+    private RadioButton pen,eraser,goback,gonext,save;
+    private View board;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
+
+    private  void initView(){
+        board=findViewById(R.id.board);
+        menu=findViewById(R.id.menu);
+        pen=findViewById(R.id.pen);
+        eraser=findViewById(R.id.eraser);
+        goback=findViewById(R.id.goback);
+        gonext=findViewById(R.id.gonext);
+        save=findViewById(R.id.save);
+    }
+
+
+
+
 }
