af11ae42d4ead1ec6e9add38d52cb5ec3bc32746
xlovelteb <qishi5261@live.cn>
2020-02-26 18:40:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/drawboard/DrawBoard.java
@@ -0,0 +1,14 @@
+package com.example.drawboard;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class DrawBoard extends View {
+    public DrawBoard(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
---
+++ app/src/main/java/com/example/drawboard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.drawboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
