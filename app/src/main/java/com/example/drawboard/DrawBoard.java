a8ecacda1e3dd3f966ff8fc8efe387c6db300e43
xlovelteb <qishi5261@live.cn>
2020-02-27 19:26:26 +0800

    add some function for the Board. eraser undo restore

--- app/src/main/java/com/example/drawboard/DrawBoard.java
+++ app/src/main/java/com/example/drawboard/DrawBoard.java
@@ -1,13 +1,11 @@
 package com.example.drawboard;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -18,41 +16,66 @@ import java.util.List;
 
 public class DrawBoard extends View {
 
-    private Paint mpaint=new Paint();
+    private Paint mpaint = new Paint();
 
-    private Canvas mcanvas;
     private Path path;
     private float mLastX;
     private float mLastY;
-    private List<Path> pathList = new ArrayList<>();
+    private int paintSize = 20;
+    private int paintColor = 0xFF000000;
 
+    private List<Path> lastList = new ArrayList<>();
+    private List<Path> pathList = new ArrayList<>();
+    private List<Integer> paintList = new ArrayList<>();
 
     public DrawBoard(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init();
     }
 
-    private void init(Context context) {
-        mcanvas = new Canvas();
-        mpaint.setColor(Color.BLACK);
+    private void init() {
+        pathList.clear();
+        mpaint.setStrokeWidth(paintSize);
+        mpaint.setColor(paintColor);
         mpaint.setStyle(Paint.Style.STROKE);
-        mpaint.setStrokeWidth(20);
         mpaint.setAntiAlias(true);
         mpaint.setStrokeCap(Paint.Cap.ROUND);
-
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        for (Path path1 : pathList) {
-            canvas.drawPath(path1, mpaint);
+//        if(currentLocation>0&&currentLocation<pathList.size()){
+//        if(!ischange)
+//            currentLocation=paintList.size();
+//            for (int i=0;i<currentLocation;i++) {
+//                Path path1=pathList.get(i);
+//                Paint paint1=new Paint();
+//                paint1.setStrokeWidth(paintSize);
+//                paint1.setColor(paintList.get(i));
+//                paint1.setStyle(Paint.Style.STROKE);
+//                paint1.setAntiAlias(true);
+//                paint1.setStrokeCap(Paint.Cap.ROUND);
+//                canvas.drawPath(path1, paint1);
+//            }
+//        }
+//        else {
+//            for (int i = 0; i < pathList.size(); i++) {
+//                Path path1 = pathList.get(i);
+//                canvas.drawPath(path1, mpaint);
+//            }
+//            currentLocation=pathList.size()-1;
+//        }
+        for (int i = 0; i < pathList.size(); i++) {
+            Path path1 = pathList.get(i);
+            Paint paint1 = new Paint();
+            paint1.setStrokeWidth(paintSize);
+            paint1.setColor(paintList.get(i));
+            paint1.setStyle(Paint.Style.STROKE);
+            paint1.setAntiAlias(true);
+            paint1.setStrokeCap(Paint.Cap.ROUND);
+            canvas.drawPath(path1, paint1);
+
         }
         canvas.save();
         canvas.restore();
@@ -64,24 +87,55 @@ public class DrawBoard extends View {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             path = new Path();
             pathList.add(path);
+            paintList.add(mpaint.getColor());
             path.moveTo(event.getX(), event.getY());
+
             mLastX = x;
             mLastY = y;
-            invalidate();
         }
         if (event.getAction() == MotionEvent.ACTION_MOVE) {
             path.quadTo(mLastX, mLastY, (mLastX + x) / 2, (mLastY + y) / 2);
             mLastX = x;
             mLastY = y;
+        }
+        invalidate();
+        return true;
+    }
+
+    public void changePaintSize(int n) {
+        this.paintSize = n;
+        invalidate();
+    }
+
+    public void changePaintColor(int color) {
+        this.paintColor = color;
+        invalidate();
+    }
+
+    public void goback() {
+        if (pathList.size() > 0) {
+            lastList.add(pathList.get(pathList.size() - 1));
+            pathList.remove(pathList.size() - 1);
+        }
+        invalidate();
+    }
+
+    public void gonext() {
+        if (lastList.size() > 0) {
+            pathList.add(lastList.get(0));
+            lastList.remove(0);
             invalidate();
         }
-//        if (event.getAction()==MotionEvent.ACTION_UP){
-//            path.quadTo(mLastX, mLastY, (mLastX + x) / 2, (mLastY + y) / 2);
-//            mcanvas.drawPath(path, mpaint);
-//            mLastX = x;
-//            mLastY = y;
-//            invalidate();
-//        }
-        return true;
+    }
+
+    public void seteraser() {
+        mpaint.setColor(Color.WHITE);
+        invalidate();
+    }
+
+    public void setpen() {
+        mpaint.setStrokeWidth(paintSize);
+        mpaint.setColor(paintColor);
+        invalidate();
     }
 }
--- app/src/main/java/com/example/drawboard/MainActivity.java
+++ app/src/main/java/com/example/drawboard/MainActivity.java
@@ -2,18 +2,34 @@ package com.example.drawboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.PrivateKey;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     private RadioGroup menu;
-    private RadioButton pen,eraser,goback,gonext,save;
-    private View board;
+    private RadioButton pen, eraser, goback, gonext, save;
+    private DrawBoard board;
+    private TextView textView;
+    private SeekBar seekBar;
+
+    private int colors[] = new int[]{0xff00ffff, 0xffff0000, 0xff00ff00, 0xff00ffff};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,17 +37,58 @@ public class MainActivity extends AppCompatActivity {
         initView();
     }
 
-    private  void initView(){
-        board=findViewById(R.id.board);
-        menu=findViewById(R.id.menu);
-        pen=findViewById(R.id.pen);
-        eraser=findViewById(R.id.eraser);
-        goback=findViewById(R.id.goback);
-        gonext=findViewById(R.id.gonext);
-        save=findViewById(R.id.save);
+    private void initView() {
+        board = findViewById(R.id.board);
+        menu = findViewById(R.id.menu);
+        pen = findViewById(R.id.pen);
+        eraser = findViewById(R.id.eraser);
+        goback = findViewById(R.id.goback);
+        gonext = findViewById(R.id.gonext);
+        save = findViewById(R.id.save);
+
+        pen.setOnClickListener(this);
+        eraser.setOnClickListener(this);
+        goback.setOnClickListener(this);
+        gonext.setOnClickListener(this);
+    }
+
+    private Bitmap getViewBitmap(View v) {
+        Bitmap bitmap = Bitmap.createBitmap(v.getWidth(), v.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        v.draw(canvas);
+        return bitmap;
+    }
+
+    private void saveBitmap(View v) {
+        Bitmap bitmap = getViewBitmap(v);
+
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.eraser:
+                board.seteraser();
+                Log.i("i","eraser");
+                break;
+            case R.id.pen:
+                board.setpen();
+                Log.i("i","pen");
 
+                break;
+            case R.id.goback:
+                board.goback();
+                Log.i("i","goback");
 
+                break;
+            case R.id.gonext:
+                board.gonext();
+                break;
+            case R.id.colors:
+                board.changePaintColor(colors[2]);
+                break;
+        }
+    }
 }
